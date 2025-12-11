b37d08e224b5285e3b2ea4d56be0245b936ee1b0
varvara-krasavina <varvara.krasavina@gmail.com>
2025-12-11 13:21:50 +0100

    Speed up ocr recognition

--- converter/converter.py
+++ converter/converter.py
@@ -1,8 +1,10 @@
 """PDF to EPUB converter utils."""
 
-import io
+import asyncio
 import logging
+import os
 import re
+from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 
 from fastapi import File
@@ -12,6 +14,8 @@ from pypandoc import convert_text as convert_text_to_epub
 from pytesseract import image_to_string
 
 logging.getLogger().setLevel("INFO")
+
+MAX_WORKERS = os.cpu_count()
 
 
 def _preprocess_text(text: str) -> str:
@@ -23,23 +27,33 @@ def _preprocess_text(text: str) -> str:
     return text
 
 
-def _images2txt(images: list[JpegImageFile], language: str) -> str:
+def _image2txt(image: JpegImageFile, language: str) -> str:
+    """Converts PIL image to a TXT file using OCR."""
+    text = image_to_string(image, lang=language)
+    return _preprocess_text(text)
+
+
+async def _images2txt(images: list[JpegImageFile], language: str) -> str:
     """Converts PIL images to a TXT file using OCR."""
-    buf = io.StringIO()
-    for image in images:
-        text = image_to_string(image, lang=language)
-        text = _preprocess_text(text)
-        buf.write(text + "\n")
-    text = buf.getvalue()
-    return text
+    loop = asyncio.get_running_loop()
+    with ProcessPoolExecutor(max_workers=MAX_WORKERS) as pool:
+        tasks = [
+            loop.run_in_executor(pool, _image2txt, image, language) for image in images
+        ]
+        text_chunks = await asyncio.gather(*tasks)
+
+    return "\n".join(text_chunks)
 
 
 async def pdf2epub(file: File, language: str) -> None:
     """Converts PDF file to a EPUB file using OCR."""
     logging.info("Processing PDF file ...")
     bytes_file = await file.read()
+    logging.info("Converting pdf to images")
     images = convert_pdf_to_pil(bytes_file, fmt="jpeg")
-    text = _images2txt(images, language)
+    logging.info("Converting images to text")
+    text = await _images2txt(images, language)
+    logging.info("Converting text to epub")
 
     convert_text_to_epub(
         text,
